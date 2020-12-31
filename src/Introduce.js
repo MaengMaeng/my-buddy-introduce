9f31cbf838d7c880c85544d3964e2b5b04d7aa7a
eunjeong <cs801458j@gmail.com>
2020-12-31 15:38:06 +0900

    [fix] head 제거

--- src/Introduce.js
+++ src/Introduce.js
@@ -3,9 +3,6 @@ import IntroduceLayout from "./IntroduceLayout";
 function Introduce() {
   return (
     <div className="Introduce">
-      <head>
-          <title>내친소</title>
-      </head>
       <IntroduceLayout>
           <div>친구소개</div>
       </IntroduceLayout>
