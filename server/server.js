2f8950bf3a4f226ff6089fcd8b2884aec0eccb8a
MaengMaeng <maybe8189@naver.com>
2021-01-04 07:40:00 +0900

    server.js passport 코드 삭제

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 require('./db')();
 
 const express = require('express');
-
-const passport = require('passport');
 const session = require('express-session');
 
 const app = express();
@@ -15,22 +13,6 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./passport');
-
-app.get('/',(req, res) => {
-    res.json(req.session.passport.user);
-})
-
-app.get('/auth/github', passport.authenticate('github'));
-
-app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
-    function (req, res) {
-        res.redirect('/');
-    });
 
 app.listen(port = 5000, () => {
     console.log(`express is running on ${port}`);
