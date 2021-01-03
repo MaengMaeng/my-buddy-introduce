b852f58d0fc2b537f94b47225725bb7043e6d1aa
MaengMaeng <maybe8189@naver.com>
2021-01-04 07:39:17 +0900

    헤더 컴포넌트 생성

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <div className='header'>
+                <div className='logo'>
+                    logo
+                </div>
+                <div className='login'>
+                    login
+                </div>
+                <div className='profile'>
+                    <div className='profile__image'>
+
+                    </div>
+                    <div className='profile__name'>
+        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
